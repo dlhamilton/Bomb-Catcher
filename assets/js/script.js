4981dc5f859af991785e19bebc450193e2f1ed56
Dan Hamilton <hamiltondan93@gmail.com>
2022-08-28 19:07:06 +0000

    Refactor: All files have been copied and the code will be rewritten for a more code friendly read. JS to show modal settings added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,217 +1,261 @@
-const element = document.getElementById("startTimer");
-const gameElement = document.getElementById("gameArea");
-const modalStartCountdownElement = document.getElementById("modalStartCountdown");
+// Wait for the Dom to finish loading before running the game 
+// Open the settings modal to get the user preference for the game
 
-let count = 3;
-var startInterval;
+document.addEventListener("DOMContentLoaded", function () {
+  let gameSettings_Modal = document.getElementById("mymodalGameSettings");
+  showElement(gameSettings_Modal);
+  let gameSettings = getModalInformation(gameSettings_Modal);
+alert(gameSettings.x);
+alert(gameSettings.y);
+alert(gameSettings.l);
+})
+/**
+ * show a modal element on the DOM by changing display properties.
+ * Then get the form data that is inputted.
+ */
+function getModalInformation(element){
+  //get form, detaisl from form
+  let x_size = 4;
+  let y_size = 4;
+  let level = 1000;
+  return {x:x_size,y:y_size,l:level};
+}
+/**
+ * Hide an element on the DOM by changing display properties
+ */
+function hideElement(element){
+  element.style.display = "none";
+}
+/**
+ * Show an element on the DOM by changing display properties
+ */
+ function showElement(element){
+  element.style.display = "block";
+}
 
-function gameReset(){
-  score = 0;
-  document.getElementById("gameScore").innerHTML = score;
-  pauseGameBtn.style.color = "whitesmoke";
-  active = [];
-  mainGame = null;
-  gametick = bombSpeed;
-  elementgrid.classList.remove("gameOverFlash");
-  for (let bomb of bombs) {
-    bomb.style.backgroundColor = "black";
-    bomb.fuse = 3;
-    bomb.addEventListener("click", () => {bomb.stopBomb(false)});
-    bomb.innerHTML=`<i class="fa-solid fa-bomb"></i>`;
-  }
-  startInterval = setInterval(startCountdown, 1000);
+
+
+
+
+
+
+// const element = document.getElementById("startTimer");
+// const gameElement = document.getElementById("gameArea");
+// const modalStartCountdownElement = document.getElementById("modalStartCountdown");
+
+// let count = 3;
+// var startInterval;
+
+// function gameReset(){
+//   score = 0;
+//   document.getElementById("gameScore").innerHTML = score;
+//   pauseGameBtn.style.color = "whitesmoke";
+//   active = [];
+//   mainGame = null;
+//   gametick = bombSpeed;
+//   elementgrid.classList.remove("gameOverFlash");
+//   for (let bomb of bombs) {
+//     bomb.style.backgroundColor = "black";
+//     bomb.fuse = 3;
+//     bomb.addEventListener("click", () => {bomb.stopBomb(false)});
+//     bomb.innerHTML=`<i class="fa-solid fa-bomb"></i>`;
+//   }
+//   startInterval = setInterval(startCountdown, 1000);
  
-}
+// }
 
-function startCountdown(){
-  modalStartCountdownElement.style.display ="block";
-    element.innerHTML = count;
-    count-=1;
-    if (count === -1){  
-    element.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
-    }
-    if(count === -2){
-      modalStartCountdownElement.style.display = "none";
-        clearInterval(startInterval);
-        gameElement.focus();
-        gameRun()
-    }
-}
+// function startCountdown(){
+//   modalStartCountdownElement.style.display ="block";
+//     element.innerHTML = count;
+//     count-=1;
+//     if (count === -1){  
+//     element.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
+//     }
+//     if(count === -2){
+//       modalStartCountdownElement.style.display = "none";
+//         clearInterval(startInterval);
+//         gameElement.focus();
+//         gameRun()
+//     }
+// }
 
-let score = 0;
-let bombSpeed = document.getElementById("myRange").value;
+// let score = 0;
+// let bombSpeed = document.getElementById("myRange").value;
 
-let bombs = document.getElementsByClassName("grid-container")[0].children;
-window.onload = gameStartValues;
- function gameStartValues() {
-for (let bomb of bombs) {
-  bomb.style.backgroundColor = "black";
-  bomb.fuse = 3;
-  bomb.colorChange = function () {
-    // this.style.backgroundColor = randomColor(this.fuse);
-    this.style.animation = "startBombColor 3s ease 0s 1";
-    if (this.fuse === -1) {
-      //alert("Game over");
-      bomb.removeEventListener("click", () => {bomb.stopBomb(false)});
-      this.style.animation = "initial";
-      this.style.backgroundColor = "pink";
-      this.innerHTML=`<i class="fa-solid fa-burst"></i>`;
-      //this.stopBomb();
-      gameOver();
-      this.style.backgroundColor = "pink";
-    };
-    this.fuse -= 1;
-  };
-  bomb.stopBomb = function (x) {
-    clearInterval(this.myVar);
-    this.fuse = 3;
-    // console.log(active);
-    // console.log("we made it : " + this.innerHTML);
-    if (this.style.backgroundColor != "pink"){
-    this.style.backgroundColor = "black";
-  }
-    this.style.animation = "initial";
-    // console.log(bomb.getAttribute("name"));
-    // console.log("hello " + active.indexOf(parseInt(bomb.getAttribute("name")) - 1));
-    let removeSpace = active.indexOf(parseInt(bomb.getAttribute("name")) - 1);
-    if (removeSpace != -1) {
-      active.splice(removeSpace, 1);
-      console.log(x);
-      if (x==true){
-      score +=1 ;
-      }
-    }
+// let bombs = document.getElementsByClassName("grid-container")[0].children;
+// window.onload = gameStartValues;
+//  function gameStartValues() {
+// for (let bomb of bombs) {
+//   bomb.style.backgroundColor = "black";
+//   bomb.fuse = 3;
+//   bomb.colorChange = function () {
+//     // this.style.backgroundColor = randomColor(this.fuse);
+//     this.style.animation = "startBombColor 3s ease 0s 1";
+//     if (this.fuse === -1) {
+//       //alert("Game over");
+//       bomb.removeEventListener("click", () => {bomb.stopBomb(false)});
+//       this.style.animation = "initial";
+//       this.style.backgroundColor = "pink";
+//       this.innerHTML=`<i class="fa-solid fa-burst"></i>`;
+//       //this.stopBomb();
+//       gameOver();
+//       this.style.backgroundColor = "pink";
+//     };
+//     this.fuse -= 1;
+//   };
+//   bomb.stopBomb = function (x) {
+//     clearInterval(this.myVar);
+//     this.fuse = 3;
+//     // console.log(active);
+//     // console.log("we made it : " + this.innerHTML);
+//     if (this.style.backgroundColor != "pink"){
+//     this.style.backgroundColor = "black";
+//   }
+//     this.style.animation = "initial";
+//     // console.log(bomb.getAttribute("name"));
+//     // console.log("hello " + active.indexOf(parseInt(bomb.getAttribute("name")) - 1));
+//     let removeSpace = active.indexOf(parseInt(bomb.getAttribute("name")) - 1);
+//     if (removeSpace != -1) {
+//       active.splice(removeSpace, 1);
+//       console.log(x);
+//       if (x==true){
+//       score +=1 ;
+//       }
+//     }
 
     
-  }
-  bomb.ignite = false;
-  bomb.addEventListener("click", () => {bomb.stopBomb(true)});
+//   }
+//   bomb.ignite = false;
+//   bomb.addEventListener("click", () => {bomb.stopBomb(true)});
 
-  bomb.endGame = function(){
-    // for (let bomb of bombs) {
-    bomb.stopBomb(false);
-    console.log("Game over" + bomb);
-    // }
-  }
+//   bomb.endGame = function(){
+//     // for (let bomb of bombs) {
+//     bomb.stopBomb(false);
+//     console.log("Game over" + bomb);
+//     // }
+//   }
 
-}
-}
+// }
+// }
 
-let active = [];
+// let active = [];
 
-// document.getElementsByTagName("body")[0].onload = function () {
-//   gameRun()
+// // document.getElementsByTagName("body")[0].onload = function () {
+// //   gameRun()
+// // };
+// let mainGame;
+// function gameRun() {
+//   let gametick = bombSpeed;
+//  mainGame = setInterval(mainGameFunc, gametick);
+//  mainGameFunc();
 // };
-let mainGame;
-function gameRun() {
-  let gametick = bombSpeed;
- mainGame = setInterval(mainGameFunc, gametick);
- mainGameFunc();
-};
-let startingTwoBombsTimer = 0;
-function mainGameFunc() {
-  document.getElementById("gameScore").innerHTML = score;
-    let j;
+// let startingTwoBombsTimer = 0;
+// function mainGameFunc() {
+//   document.getElementById("gameScore").innerHTML = score;
+//     let j;
    
-    if(startingTwoBombsTimer>=2){
-     gametick = 0;
-    }else{
-      startingTwoBombsTimer += 1;
-    }
-    if (active.length < 3) {
-      do {
-        j = Math.floor(Math.random() * 9);
-      } while (active.indexOf(j) != -1);
-      active.push(j);
+//     if(startingTwoBombsTimer>=2){
+//      gametick = 0;
+//     }else{
+//       startingTwoBombsTimer += 1;
+//     }
+//     if (active.length < 3) {
+//       do {
+//         j = Math.floor(Math.random() * 9);
+//       } while (active.indexOf(j) != -1);
+//       active.push(j);
      
-      bombs[j].myVar = setInterval(myTimer, bombSpeed, j);
-      console.log("run " + j);
-    }
+//       bombs[j].myVar = setInterval(myTimer, bombSpeed, j);
+//       console.log("run " + j);
+//     }
    
-  }
+//   }
 
-function myTimer(i) {
-  bombs[i].colorChange();
-}
-const elementend = document.getElementById("gameOverScreen");
-// const elementendmodal = document.getElementById("gameOverModal");
-const elementgrid = document.getElementById("gameArea");
-function gameOver(){
-// elementend.innerHTML = "Game Over";
-// elementendmodal.style.display = "block";
-elementgrid.classList.add("gameOverFlash");
-for (let bomb of bombs) {
- bomb.removeEventListener("click", () => {bomb.stopBomb(false)});
-bomb.endGame();
- };
-console.log("sass");
-  clearInterval(mainGame);
-}
+// function myTimer(i) {
+//   bombs[i].colorChange();
+// }
+// const elementend = document.getElementById("gameOverScreen");
+// // const elementendmodal = document.getElementById("gameOverModal");
+// const elementgrid = document.getElementById("gameArea");
+// function gameOver(){
+// // elementend.innerHTML = "Game Over";
+// // elementendmodal.style.display = "block";
+// elementgrid.classList.add("gameOverFlash");
+// for (let bomb of bombs) {
+//  bomb.removeEventListener("click", () => {bomb.stopBomb(false)});
+// bomb.endGame();
+//  };
+// console.log("sass");
+//   clearInterval(mainGame);
+// }
 
-// Get the modal
-var modalGameSettings = document.getElementById("mymodalGameSettings");
-modalGameSettings.style.display = "block";
+// // Get the modal
+// var modalGameSettings = document.getElementById("mymodalGameSettings");
+// modalGameSettings.style.display = "block";
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var modalGameSettingsbtn = document.getElementById("modalGameSettingsOpen");
-// When the user clicks the button, open the modal 
-modalGameSettingsbtn.onclick = function() {
-  modalGameSettings.style.display = "block";
-}
+// // Get the button that opens the modal
+// var modalGameSettingsbtn = document.getElementById("modalGameSettingsOpen");
+// // When the user clicks the button, open the modal 
+// modalGameSettingsbtn.onclick = function() {
+//   modalGameSettings.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modalGameSettings.style.display = "none";
-  modalStartCountdownElement.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modalGameSettings.style.display = "none";
+//   modalStartCountdownElement.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modalGameSettings) {
-    modalGameSettings.style.display = "none";
-    modalStartCountdownElement.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modalGameSettings) {
+//     modalGameSettings.style.display = "none";
+//     modalStartCountdownElement.style.display = "none";
+//   }
+// }
 
-// Get the button that opens the modal
-var staraGame = document.getElementById("startGameBtn");
-// When the user clicks the button, open the modal 
-staraGame.onclick = function() {
-  document.getElementById("mymodalGameSettings").style.display = "none";
-  modalStartCountdownElement.style.display = "none";
-  count=3;
-  gameReset();
-}
+// // Get the button that opens the modal
+// var staraGame = document.getElementById("startGameBtn");
+// // When the user clicks the button, open the modal 
+// staraGame.onclick = function() {
+//   document.getElementById("mymodalGameSettings").style.display = "none";
+//   modalStartCountdownElement.style.display = "none";
+//   count=3;
+//   gameReset();
+// }
 
-// Get the button that redo the game
-var redoGameBtn = document.getElementById("redoGame");
-// When the user clicks the button start
-redoGameBtn.onclick = function() {
-  document.getElementById("mymodalGameSettings").style.display = "none";
-  modalStartCountdownElement.style.display = "none";
-  count=3;
-  gameReset();
-}
-// Get the button that redo the game
-var pauseGameBtn = document.getElementById("pauseGame").children[0];
-// When the user clicks the button start
-pauseGameBtn.onclick = function() {
-  if (pauseGameBtn.style.color ==="red"){
-    pauseGameBtn.style.color = "whitesmoke";
-    startingTwoBombsTimer = 0;
-    gametick = bombSpeed;
-    mainGame = setInterval(mainGameFunc, gametick);
-  }else{
-    pauseGameBtn.style.color = "red";
-    for(let bomb of bombs){
-      if(bomb.myVar===undefined || bomb.myVar ===null){
+// // Get the button that redo the game
+// var redoGameBtn = document.getElementById("redoGame");
+// // When the user clicks the button start
+// redoGameBtn.onclick = function() {
+//   document.getElementById("mymodalGameSettings").style.display = "none";
+//   modalStartCountdownElement.style.display = "none";
+//   count=3;
+//   gameReset();
+// }
+// // Get the button that redo the game
+// var pauseGameBtn = document.getElementById("pauseGame").children[0];
+// // When the user clicks the button start
+// pauseGameBtn.onclick = function() {
+//   if (pauseGameBtn.style.color ==="red"){
+//     pauseGameBtn.style.color = "whitesmoke";
+//     startingTwoBombsTimer = 0;
+//     gametick = bombSpeed;
+//     mainGame = setInterval(mainGameFunc, gametick);
+//   }else{
+//     pauseGameBtn.style.color = "red";
+//     for(let bomb of bombs){
+//       if(bomb.myVar===undefined || bomb.myVar ===null){
         
-      }
-      bomb.stopBomb(false);
-    }
- clearInterval(mainGame);
-  }
-}
+//       }
+//       bomb.stopBomb(false);
+//     }
+//  clearInterval(mainGame);
+//   }
+// }
+
+
+
